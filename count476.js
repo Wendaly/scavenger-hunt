da4d313e1e25e7342f8d653d394a6501498b5057
wendaly <wtaveras136@gmail.com>
2016-10-05 21:48:47 +0000

    saving

---
+++ count476.js
@@ -0,0 +1,3 @@
+for(var i=0;i>477;i++){
+    console.log(i)
+};
---
+++ helloName.js
@@ -0,0 +1,6 @@
+var person = prompt("what is your name?");
+
+if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person;
+}
